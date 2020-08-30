009e522e9e7ffd52c9cd1aab30c87b5447009aae
Keyur Lakhani <keyur.lakhani99@gmail.com>
2020-08-30 11:22:51 -0400

    new file

--- app/directives/newDateMask.js
+++ app/directives/newDateMask.js
@@ -1,225 +1,230 @@
-define(['app'], function (app) {
-    app.directive('dateInputMask', function () {
-      var pattern = '99/99/9999';
-      var mask = '99/99/9999';
-      var kb = {};
-      kb.backspace = 'Backspace';
-      kb.arrowLeft = 'ArrowLeft';
-      kb.arrowRight = 'ArrowRight';
-      kb.home = 'Home';
-      kb.end = 'End';
-      kb.tab = 'Tab';
-  
-      var format = 'MM/DD/YYYY';
-      var separator = '/'
-      var formats = format.split('/')
-      var formatLengthByIndex = [];
-      var validatorByIndex = [];
-      var fLength = formats.reduce((p, c) => p + c.length, 0) + formats.length - 1;
-      formats.forEach(function (f) {
-        var pos = format.indexOf(f);
-        validator = function(){};
-        for (var index = pos; index < pos + f.length; index++) {
-          formatLengthByIndex[index] = f.length;
-          validatorByIndex[index] = validator
-        }
-      })
-  
-      return {
-        restrict: 'A',
-        require: '?ngModel',
-        link: dateInputMaskLinkFn
+directive('dateInputMask', dateInputMaskDirective);
+
+  function dateInputMaskDirective() {
+    var pattern = '99/99/9999';
+    var mask = '99/99/9999';
+    var kb = {};
+    kb.backspace = 'Backspace';
+    kb.arrowLeft = 'ArrowLeft';
+    kb.arrowRight = 'ArrowRight';
+    kb.home = 'Home';
+    kb.end = 'End';
+    kb.tab = 'Tab';
+
+    var format = 'MM/DD/YYYY';
+    var separator = '/'
+    var formats = format.split('/')
+    var formatLengthByIndex = [];
+    var validatorByIndex = [];
+    var fLength = formats.reduce((p, c) => p + c.length, 0) + formats.length - 1;
+    formats.forEach(function (f) {
+      var pos = format.indexOf(f);
+      for (var index = pos; index < pos + f.length; index++) {
+        formatLengthByIndex[index] = f.length;
       }
-  
-  
-      function dateInputMaskLinkFn(scope, element, attrs, ctrl) {
-        if (!ctrl) {
+    })
+
+    return {
+      restrict: 'A',
+      require: '?ngModel',
+      link: dateInputMaskLinkFn
+    }
+
+    function dateInputMaskLinkFn(scope, element, attrs, ctrl) {
+      if (!ctrl) {
+        return;
+      }
+
+      element.on('keydown', onKeyDown);
+      element.on('input', onInput);
+
+      ctrl.$validators.date = function validator(modelValue, viewValue) {
+        if (ctrl.$isEmpty(modelValue)) {
+          return true;
+        }
+        const futureDate = new Date()
+        const currDate = new Date(viewValue);
+        return currDate.toDateString() !== 'Invalid Date'
+          && viewValue.length === format.length
+          && currDate.getTime() <= futureDate.getTime()
+      };
+
+      function isNumber(number) {
+        return /^\d$/g.test(number);
+      }
+      //  removeLast(2 + input.value.length - index);
+      function removeLast(count) {
+        const inputValueArray = input.value.split('');
+
+        for (let index = 0; index < count; index++) {
+          inputValueArray.pop();
+        }
+
+        input.value = inputValueArray.join('');
+      }
+
+      function onKeyDown(event) {
+        var input = event.target;
+        if (event.key === kb.arrowLeft
+          || event.key === kb.arrowRight
+          || event.key === kb.home
+          || event.key === kb.end) {
           return;
         }
-  
-        var input = element[0];
-  
-        element.on('keydown', onKeyDown);
-        element.on('input', onInput);
-  
-        function isNumber(number) {
-          return /^\d$/g.test(number);
+        var index = input.selectionStart;
+        if (event.key === kb.tab) {
+          return false;
         }
-  
-        function removeLast(count) {
-          const inputValueArray = input.value.split('');
-  
-          for (let index = 0; index < count; index++) {
-            inputValueArray.pop();
-          }
-  
-          input.value = inputValueArray.join('');
-        }
-  
-        function onKeyDown(event) {
-          var input = element[0];
-          if (event.key === kb.arrowLeft
-            || event.key === kb.arrowRight
-            || event.key === kb.home
-            || event.key === kb.end) {
-            return;
-          }
-          var index = input.selectionStart;
-          if (event.key === kb.tab) {
+        if (event.key !== kb.backspace) {
+          if (!isNumber(event.key) || index === fLength) {
             return false;
           }
-          if (event.key !== kb.backspace) {
-            if (!isNumber(event.key) || index === fLength) {
+        } else {
+          if (!input.value[index - 1]) {
+            return false;
+          }
+          var separatorExistsAfterwards = !!input.value
+            .split('')
+            .filter((_, i) => i >= index)
+            .find(i => i === separator);
+
+          if (separatorExistsAfterwards) {
+            if (input.value[index - 1] === separator) {
               return false;
             }
           } else {
-            if (!input.value[index - 1]) {
+            if (input.value[index - 2] === separator) {
+              removeLast(2 + input.value.length - index);
               return false;
             }
-            var separatorExistsAfterwards = !!input.value
-              .split('')
-              .filter((_, i) => i >= index)
-              .find(i => i === separator);
-  
-            if (separatorExistsAfterwards) {
-              if (input.value[index - 1] === separator) {
-                return false;
-              }
-            } else {
-              if (input.value[index - 2] === separator) {
-                removeLast(2 + input.value.length - index);
-                return false;
-              }
-            }
-            return;
           }
-  
-          var delta = deltaFn(index);
-          var currentIndex = index;
-          if ((input.value[index + 1] && input.value[index] !== separator) 
-            || (input.value[index] && index + delta === fLength - 1)) {
-            var inputValueArray = input.value.split('');
-  
-            for (let i = 0; i < inputValueArray.length; i++) {
-              if (i === index) {
-                inputValueArray[i] = event.key;
-  
-                for (let current = i + 1; input.value[current] !== separator 
-                    && current < fLength; current++) {
-                  inputValueArray[current] = undefined;
-                }
-              }
-            }
-  
-            input.value = inputValueArray.filter(i => i).join('');
-            console.log('value', input.value)
-            setCaretPosition(index + 1);
-  
-            const formatLength = formatLengthFn(index);
-  
-            if (formatLength === formatLengthByIndex[index + delta]) {
-              input.dispatchEvent(new Event('change'));
-            }
-  
-            return false;
-          }
-          var formatLength = formatLengthFn(index);
-  
-          if (formatLength === formatLengthByIndex[index + delta]) {
-            return false;
-          }
+          return;
         }
-  
-        function onInput(event) {
-          var index = event.target.selectionStart;
-          var delta = deltaFn(index);
-          var isBeforeEnd = index + delta < fLength;
-          console.log('before input value', input.value)
-          if (!validatorByIndex[index + delta] 
-              && isBeforeEnd 
-              && input.value.length < fLength 
-              && event.inputType !== 'deleteContentBackward') {
-            input.value += separator;
+
+        var delta = deltaFn(index, input);
+        var currentIndex = index;
+        if ((input.value[index + 1] && input.value[index] !== separator)
+          || (input.value[index] && index + delta === fLength - 1)) {
+          var inputValueArray = input.value.split('');
+
+          for (let i = 0; i < inputValueArray.length; i++) {
+            if (i === index) {
+              inputValueArray[i] = event.key;
+
+              for (let current = i + 1; input.value[current] !== separator
+                && current < fLength; current++) {
+                inputValueArray[current] = undefined;
+              }
+            }
           }
-  
-          console.log('after input value', input.value);
-          const formatLength = formatLengthFn(index);
-  
-          if (formatLength === formatLengthByIndex[index + delta - 1]) {
+
+          input.value = inputValueArray.filter(i => i).join('');
+          console.log('value', input.value)
+          setCaretPosition(index + 1);
+
+          const formatLength = formatLengthFn(index, input);
+
+          if (formatLength === formatLengthByIndex[index + delta]) {
             input.dispatchEvent(new Event('change'));
           }
+
+          return false;
         }
-  
-        function formatLengthFn(index) {
-          var formatLength = 0;
-      
-          for (var ix = index - 1; input.value[ix] && input.value[ix] !== separator && ix >= 0; ix--) {
-            formatLength++;
-          }
-      
-          for (var ix = index; input.value[ix] && input.value[ix] !== separator && ix < fLength; ix++) {
-            formatLength++;
-          }
-      
-          return formatLength;
+        var formatLength = formatLengthFn(index, input);
+
+        if (formatLength === formatLengthByIndex[index + delta]) {
+          return false;
         }
-  
-        function setCaretPosition(pos) {
-          setCaretSelection(pos, pos);
-        }
-  
-        function setCaretSelection(start, end) {
-          // Modern browsers
-          if (input.setSelectionRange) {
-            input.focus();
-            input.setSelectionRange(start, end);
-  
-            // IE8 and below
-          } else if (input.createTextRange) {
-            var range = input.createTextRange();
-            range.collapse(true);
-            range.moveEnd('character', start);
-            range.moveStart('character', end);
-            range.select();
-          }
-        }
-  
-        function deltaFn(index) {
-          let delta = 0;
-  
-          if (input.value) {
-            var previousSeparators = input.value
-              .split('')
-              .filter((_, i) => i < index)
-              .filter(i => i === separator);
-  
-            var separatorsCount = previousSeparators.length;
-            var subdelta = 0;
-  
-            if (input.value[index] === separator) {
-              separatorsCount++;
-              subdelta++;
-            }
-  
-            if (separatorsCount === 2) {
-              const inputLastIndex = input.value.lastIndexOf(separator);
-              const formatLastIndex = format.lastIndexOf(separator);
-  
-              delta = format.substring(0, formatLastIndex).length - input.value.substring(0, inputLastIndex).length - subdelta;
-            } else if (separatorsCount === 1) {
-              const inputIndex = input.value.indexOf(separator);
-              const formatIndex = format.indexOf(separator);
-  
-              delta = format.substring(0, formatIndex).length - input.value.substring(0, inputIndex).length - subdelta;
-            }
-          }
-  
-          return delta;
-        }
-  
       }
-  
-    })
-  
-  
-  })
+
+      function onInput(event) {
+        var input = event.target;
+        var index = input.selectionStart;
+        var delta = deltaFn(index, input);
+        var isBeforeEnd = index + delta < fLength;
+        console.log('before input value', input.value)
+        if (!formatLengthByIndex[index + delta]
+          && isBeforeEnd
+          && input.value.length < fLength
+          && event.inputType !== 'deleteContentBackward') {
+          input.value += separator;
+        }
+
+        console.log('after input value', input.value);
+        const formatLength = formatLengthFn(index, input);
+
+        if (formatLength === formatLengthByIndex[index + delta - 1]) {
+          input.dispatchEvent(new Event('change'));
+        }
+      }
+
+      function formatLengthFn(index, input) {
+        var formatLength = 0;
+
+        for (var ix = index - 1; input.value[ix] && input.value[ix] !== separator && ix >= 0; ix--) {
+          formatLength++;
+        }
+
+        for (var ix = index; input.value[ix] && input.value[ix] !== separator && ix < fLength; ix++) {
+          formatLength++;
+        }
+
+        return formatLength;
+      }
+
+      function setCaretPosition(pos) {
+        setCaretSelection(pos, pos);
+      }
+
+      function setCaretSelection(start, end) {
+        // Modern browsers
+        if (input.setSelectionRange) {
+          input.focus();
+          input.setSelectionRange(start, end);
+
+          // IE8 and below
+        } else if (input.createTextRange) {
+          var range = input.createTextRange();
+          range.collapse(true);
+          range.moveEnd('character', start);
+          range.moveStart('character', end);
+          range.select();
+        }
+      }
+
+      function deltaFn(index, input) {
+        let delta = 0;
+
+        if (input.value) {
+          var previousSeparators = input.value
+            .split('')
+            .filter((_, i) => i < index)
+            .filter(i => i === separator);
+
+          var separatorsCount = previousSeparators.length;
+          var subdelta = 0;
+
+          if (input.value[index] === separator) {
+            separatorsCount++;
+            subdelta++;
+          }
+
+          if (separatorsCount === 2) {
+            const inputLastIndex = input.value.lastIndexOf(separator);
+            const formatLastIndex = format.lastIndexOf(separator);
+
+            delta = format.substring(0, formatLastIndex).length - input.value.substring(0, inputLastIndex).length - subdelta;
+          } else if (separatorsCount === 1) {
+            const inputIndex = input.value.indexOf(separator);
+            const formatIndex = format.indexOf(separator);
+
+            delta = format.substring(0, formatIndex).length - input.value.substring(0, inputIndex).length - subdelta;
+          }
+        }
+
+        return delta;
+      }
+
+    }
+
+  }
